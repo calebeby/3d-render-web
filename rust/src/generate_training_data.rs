f0f1d0607a3ce3451562cabef9bbd78307d2bced
Caleb Eby <caleb.eby01@gmail.com>
2022-05-21 23:32:55 -0700

    Make generate_training_data use bfs shallowly and dfs deeply

--- rust/src/generate_training_data.rs
+++ rust/src/generate_training_data.rs
@@ -12,9 +12,10 @@ use rand::thread_rng;
 use rand::Rng;
 use serde::Deserialize;
 use serde::Serialize;
-use solver::LookaheadSolver;
 use solver::Solver;
+use solver::{FullSearchSolver, FullSearchSolverOpts, LookaheadSolver, LookaheadSolverOpts};
 use std::io::Write;
+use std::rc::Rc;
 
 #[derive(Serialize, Deserialize)]
 struct Record {
@@ -23,15 +24,21 @@ struct Record {
 }
 
 fn main() {
-    let puzzle = puzzles::rubiks_cube_2x2();
-    let depth = 14;
+    let puzzle = Rc::new(puzzles::rubiks_cube_2x2());
+    let bfs_depth = 8;
+    let dfs_depth = 12;
 
-    let solver = LookaheadSolver::new(&puzzle, depth);
+    let bfs_solver =
+        Solver::<LookaheadSolver>::new(puzzle.clone(), LookaheadSolverOpts { depth: bfs_depth });
+    let dfs_solver =
+        Solver::<FullSearchSolver>::new(puzzle.clone(), FullSearchSolverOpts { depth: dfs_depth });
     let mut rng = thread_rng();
     let mut file = std::fs::OpenOptions::new()
         .append(true)
         .open("2x2_training_data.csv")
         .unwrap();
+
+    let solved_state = puzzle.get_initial_state();
 
     for i in 0..10000 {
         let mut csv_writer = csv::WriterBuilder::new()
@@ -41,20 +48,28 @@ fn main() {
 
         let scramble_turns = rng.gen_range(20..=30);
 
-        let initial_state = puzzle.scramble(&puzzle.get_initial_state(), scramble_turns);
+        let initial_state = puzzle.scramble(&solved_state, scramble_turns);
         println!(
             "Initial: {:.1}% solved",
             puzzle.get_num_solved_pieces(&initial_state) as f64 / puzzle.get_num_pieces() as f64
                 * 100.0
         );
-        let turns: Vec<_> = solver.next_move_iter(&puzzle, &initial_state).collect();
+        let mut turns: Vec<_> = bfs_solver.solve(initial_state.clone()).collect();
+        let mut end_state = puzzle.get_derived_state_from_turns_iter(&initial_state, turns.iter());
+        if end_state != solved_state {
+            println!("falling back to dfs");
+            turns = dfs_solver.solve(initial_state.clone()).collect();
+            end_state = puzzle.get_derived_state_from_turns_iter(&initial_state, turns.iter());
+        }
+        if end_state != solved_state {
+            panic!("not solved");
+        }
         println!("turns: {:?}", turns);
         println!(
             "scramble: {} turns, solve: {} turns",
             scramble_turns,
             turns.len()
         );
-        let end_state = puzzle.get_derived_state_from_turns_iter(&initial_state, turns.iter());
         println!(
             "Final:   {:.1}% solved",
             puzzle.get_num_solved_pieces(&end_state) as f64 / puzzle.get_num_pieces() as f64
